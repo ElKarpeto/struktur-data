0453fd03270d37fdf1290ac5e360777a3012ff18
elkarpeto <daffa1mubarok@gmail.com>
2024-03-06 12:15:04 +0700

    coba git

---
+++ belajar/coba.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct list
+{
+    int data;
+    list *next;
+};
+
+struct
+{
+    /* data */
+};
+
+class List
+{
+    int val;
+    List *next;
+
+    List(int x)
+    {
+        val = x;
+        next = nullptr;
+    }
+};
+
+int main()
+{
+}
